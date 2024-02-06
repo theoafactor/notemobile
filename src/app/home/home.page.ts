0a12eb5f98391365f9de0ece61e038fe85fea37c
Olu Adeyemo <theoafactor@gmail.com>
2024-02-06 17:18:27 +0100

    Added photo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController, LoadingController } from '@ionic/angular';
-import { Camera, CameraResultType, CameraSource, CameraDirection } from '@capacitor/camera';
+import { Camera, Photo, CameraResultType, CameraSource, CameraDirection } from '@capacitor/camera';
+import { AngularFireStorage } from "@angular/fire/compat/storage"
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,10 @@ export class HomePage {
 
   public image_link: any;
 
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController, private loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, private loadingCtrl: LoadingController, 
+  private storage: AngularFireStorage
+    
+    ) {
 
     this.loadingCtrl.create({
       message: "Please wait ...",
@@ -46,10 +50,70 @@ export class HomePage {
 
 
       //save the image for previewing 
-      this.image_link = image.webPath;
+      this.image_link = await image.webPath;
 
       console.log(this.image_link)
-   
+
+      // convert to base64 
+      let base64_image = await this.convertToBase64(image)
+
+      console.log("base64 image: ", base64_image)
+
+
+      const response = await fetch(image.webPath!)
+
+      const blob = await response.blob();
+
+      let storage_ref = this.storage.ref(`profile_photos/users_images`);
+      
+      storage_ref.put(blob).then(() => {
+          console.log("Image uploaded...")
+
+          storage_ref.getDownloadURL().subscribe(( url: any ) => {
+              console.log("Download URL: ", url)
+          })
+
+
+      })
 
   }
+
+
+  async convertToBase64(image: Photo){
+
+    const blobData = await this.readAsBase64(image)
+
+    return blobData;
+  }
+
+  async readAsBase64(image: Photo){
+    const response = await fetch(image.webPath!)
+
+    const blob = await response.blob();
+
+    console.log("Blob: ", blob)
+
+    return await this.convertBlobToBase64(blob) as string;
+
+  }
+
+
+  convertBlobToBase64(blob: Blob){
+    return new Promise((resolve, reject) => {
+
+      const reader = new FileReader();
+
+      reader.onerror = reject;
+
+      reader.onload = () => {
+          resolve(reader.result)
+      };
+
+      reader.readAsDataURL(blob)
+      
+    })
+  }
+
+
+
 }
